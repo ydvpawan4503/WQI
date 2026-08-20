f91282d9a54546a5c83e7cfa4ef6ce95926e9da8
Pawan Yadav <pawanyadav4503@gmail.com>
2026-08-21 01:54:59 +0530

    enable scheduling

--- src/main/java/in/py/main/WaterQualityMonitoringApplication.java
+++ src/main/java/in/py/main/WaterQualityMonitoringApplication.java
@@ -2,8 +2,10 @@ package in.py.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class WaterQualityMonitoringApplication {
 
 	public static void main(String[] args) {
